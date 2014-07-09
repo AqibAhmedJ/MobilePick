290a5f0a35788382fece6f642dcfcf4666183562
AqibAhmedJ <aqib.bangaloreguy@gmail.com>
2014-07-09 15:12:40 -0400

    added new file

---
+++ price.java
@@ -0,0 +1,47 @@
+class MobileSpecification
+  {
+   private name;
+   screenSize;
+   battery;
+   camera;
+   processor;
+   price;
+   
+   /*
+      input :   
+      output : Mobile name
+      Description :  Displays the name of the Handset
+    */
+       setname()
+
+       /*
+      input :   
+      output : Mobiles screensize in inches
+      Description :  Displays the screen size of the Handset
+    */
+    setScreenSize()
+    /*
+      input :   
+      output : Battery capacity in mAh
+      Description :  Displays the usage duration of a mobile 
+    */
+    setBattery()
+    /*
+      input :   
+      output : camera quality in mp
+      Description :  Displays the picture quality of camera of a mobile 
+    */
+    setCamera()
+    /*
+      input :   
+      output : processing speed
+      Description :  Displays the speed and no of cores of a mobile 
+    */
+    setProcessor()
+    /*
+      input :   
+      output : price
+      Description :  Displays the price of a mobile 
+    */
+    setPrice()
+    
